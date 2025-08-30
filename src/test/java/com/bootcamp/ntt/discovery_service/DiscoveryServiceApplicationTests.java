0e3a29d395cfe8aa9ff51ea97c74f5419ec4971b
Mario Espinoza Ponte <maed180103@gmail.com>
2025-08-30 18:42:41 -0500

    Client dependency deleted and server configuration terminated

---
+++ src/test/java/com/bootcamp/ntt/discovery_service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.ntt.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
